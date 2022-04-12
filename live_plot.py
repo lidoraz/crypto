3aba08554ae07cba4f469cc695b322a2a31fbff5
lidor-upstream <77963956+lidor-upstream@users.noreply.github.com>
2022-04-12 18:05:57 +0300

    disabled switch

--- live_plot.py
+++ live_plot.py
@@ -27,7 +27,8 @@ coin_html = html.Div(dcc.Dropdown(COINS, COINS[0], id='coin-type', clearable=Fal
 live_update_html = html.Div(id='live-update-text', style={'margin': 'auto'}, children="")  # 'width': '20%',
 resample_selector_html = dcc.RadioItems(options=resample_radio_options, value=resample_keywords[2], id='resample-type',
                                         inline=True)
-live_update_switch_html = dbc.Checklist(options=[{"label": "Live Update", "value": 1}], value=[1],
+# https://dash-bootstrap-components.opensource.faculty.ai/docs/components/input/ # RadioItems and Checklist
+live_update_switch_html = dbc.Checklist(options=[{"label": "Live Update", "value": 1, "disabled": True}], value=[],
                                         id="live-update-button", switch=True)
 
 right_portion_html = html.Div(id='right-portion',
@@ -47,7 +48,7 @@ app.layout = html.Div([
     ),
     html.Div([
         dcc.Graph(id='live-update-graph', config={'scrollZoom': True, 'displayModeBar': False}),
-        dcc.Interval(id='interval-component', interval=5 * 1000, n_intervals=0)
+        dcc.Interval(id='interval-component', interval=5 * 1000, n_intervals=0, disabled=False)
     ])
 ])
 
