c1db838491694455cd76917bca0c777e8491f8f7
shushu <shushu44832@gmail.com>
2022-04-26 01:37:37 +0300

    small fixes

--- AdvancedAnalytics/Strategy/Strategies/utils.py
+++ AdvancedAnalytics/Strategy/Strategies/utils.py
@@ -1,8 +1,9 @@
+from AdvancedAnalytics.Strategy.data_wrappers import DataWrapper
 from Crypto.DataProcessing.DataProvider import TIME_CONV
 
 
 # TODO: Generalize this with other strategies
-def mark_enter_exit_points(data_class, indicator_func, params):
+def mark_enter_exit_points(data_class: DataWrapper, indicator_func, params):
     """
     # BUY ENTRY: First candle that closes on the middle BB, soon after RSI was < 30
     # SELL ENTRY: First candle that close below the middle BB, soon after RSI was > 70
--- AdvancedAnalytics/Strategy/runner.py
+++ AdvancedAnalytics/Strategy/runner.py
@@ -3,30 +3,32 @@ from data_wrappers import CryptoData, NasdaqData
 import numpy as np
 
 
-def run_crypto():
+def get_crypto():
     from Crypto.DataProcessing.data_consts import COINS, START_DATA_DATE
     from Crypto.DataProcessing.data_utils import get_data_providers, prepare_data
-    params = {'time_intervals': ['15Min', '1H'],
+    params = {'time_intervals': ['1H'],  # ['15Min', '1H']
               'lookaheads': range(10, 17, 2),
-              'profit_percents': [round(x, 2) for x in np.arange(0.03, 0.15, 0.02)]}
+              'profit_percents': [round(x, 2) for x in np.arange(0.03, 0.15, 0.05)]}
     providers = get_data_providers()
     df_prices, df_agg = prepare_data(providers, START_DATA_DATE)
-    data_class = CryptoData(df_prices, COINS)
-    find_optimal_BBRSI_strategy(data_class, params)
+    data_wrapper = CryptoData(df_prices, df_agg, COINS)
+    return data_wrapper, params
 
 
-def run_nasdaq():
+def get_nasdaq():
     from Nasdaq.symbols import NASDAQ_PREPATH, nasdq_100
     params = {'time_intervals': [1],  # Nasdaq Daily data
               'lookaheads': range(10, 17, 2),
               'profit_percents': [round(x, 2) for x in np.arange(0.03, 0.15, 0.02)]}
     start_date = '2019-01-01'
     symbols = nasdq_100[1:10]
-    data = NasdaqData(symbols, NASDAQ_PREPATH, start_date=start_date)
-    find_optimal_BBRSI_strategy(data, params)
+    data_wrapper = NasdaqData(symbols, NASDAQ_PREPATH, start_date=start_date)
+    return data_wrapper, params
 
 
 if __name__ == '__main__':
-    # run_crypto()
+    # data_wrapper, params = get_crypto()
 
-    run_nasdaq()
+    data_wrapper, params = get_nasdaq()
+
+    find_optimal_BBRSI_strategy(data_wrapper, params)
