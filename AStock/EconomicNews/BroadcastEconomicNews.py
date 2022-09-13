eee3f8c0d6315a875f7bf2efdf77722825e8e5ab
shushu <shushu33434@gmail.com>
2022-09-13 18:29:41 +0300

    added thestrat, still not not finished in stocks added start of swing scanner

---
+++ AStock/EconomicNews/BroadcastEconomicNews.py
@@ -0,0 +1,124 @@
+# TELEGRAM_GROUP_NEWS
+import schedule
+import pandas as pd
+from AStock.EconomicNews.util import scrape_data
+from datetime import datetime
+import requests
+import os
+import time
+
+
+# df = df['dt'].dt.date
+
+
+def get_todays(tz=None):
+    try:
+        df = scrape_data()
+        print('Got table')
+        df['dt'] = df['dt'].dt.tz_localize('UTC').dt.tz_convert(tz)
+        df = df[df['dt'].dt.date == datetime.now().date()]
+        print('Todays:')
+        print(df.to_string())
+        return df
+    except Exception as e:
+        print('Something went wrong..', e)
+        return None
+
+
+def publish(msg, prod):
+    if prod:
+        url = "https://api.telegram.org/bot{token}/sendMessage?chat_id={group_id}&text={msg}&parse_mode=HTML"
+        token = os.environ.get('TELEGRAM_TOKEN')
+        group_id = os.environ.get('TELEGRAM_GROUP_NEWS')
+        print('token:', token)
+        print('group_id', group_id)
+        print(url.format(token=token, group_id=group_id, msg=msg))
+        res = requests.get(url.format(token=token, group_id=group_id, msg=msg))
+        print(res.status_code)
+        print(res.content)
+    else:
+        print(msg)
+
+
+def build_str(df, convert_tz=None):
+    df['dt'] = df['dt'].dt.tz_localize('UTC').dt.tz_convert(convert_tz)
+    today = str(df['dt'].dt.date[0])
+    str_build = f'<u><b>Todays ({today})</b></u>\n'
+    for _, row in df.iterrows():
+        importance = row['importance']
+        hour = row['dt'].hour
+        minute = row['dt'].minute
+        title = row['title']
+        consensus = row['consensus']
+        actual = row['actual']
+        importance_icon = {'1': '1', '2': '🛎️', '3': '🚨'}
+        importance = importance_icon.get(importance, '')
+        compare_icon = {True: '⬆️', False: '⬇️'}
+        compare = compare_icon.get(row['compare'], '')
+
+        actual_str = f', act:{actual}{compare}' if actual != 'NOTYET' else ''
+
+        str_build += f'{importance}{hour}:{minute:02d} {title} <b>con:{consensus}{actual_str} </b>\n'
+    return str_build
+
+
+def job_that_executes_once(hour, minute):
+    def run():
+        print('Do once', hour, minute, datetime.now())
+        df = get_todays()
+        df = df[(df['dt'].dt.hour == hour) & (df['dt'].dt.minute == minute)]
+        if len(df):
+            str_build = build_str(df, convert_tz='Israel')
+            publish(str_build, prod=True)
+            # broadcast only results from that specifiec task
+        else:
+            print('job_that_executes_once got empty Dataframe after filtering!')
+        return schedule.CancelJob
+
+    return run
+
+
+def create_tasks(df):
+    df = df[df['importance'] == '2']
+    time_now = datetime.utcnow()
+    dts = df.dt.unique()
+    for dt in dts:
+        dt = pd.Timestamp(dt)
+        if time_now < dt:
+            hour = dt.hour
+            minute = dt.minute
+            # schedule does not support timezone, must make sure the trigger time is UTC!
+            # hour = hour + 3
+            print('Registering a job to trigger today at time:', dt, f'{hour:02d}:{minute:02d}')
+            schedule.every().day.at(f'{hour:02d}:{minute:02d}:01').do(job_that_executes_once(hour, minute))
+
+
+def job():
+    df = get_todays(None)
+
+    create_tasks(df)  # Make this work, later....
+    str_build = build_str(df, convert_tz='Israel')
+    publish(str_build, prod=True)
+    # Build a task to  get data at correct timing
+
+
+def get_local_tz():
+    return int(datetime.utcnow().astimezone().utcoffset().total_seconds() / (60 * 60))
+
+
+def run_forever():
+    # at_time = '18:00'
+    at_time = '07:00'  # UTC
+    print('Running forever...')
+    publish("Test", prod=True)
+    schedule.every().day.at(at_time).do(job)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+        # schedule.get_jobs()
+
+
+if __name__ == '__main__':
+    run_forever()
+    # job()
---
+++ AStock/EconomicNews/util.py
@@ -0,0 +1,93 @@
+url = 'https://tradingeconomics.com/calendar'
+
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup as bs
+
+
+def _parse_cnt_price(val):
+    multiplyer = {'K': 1e3, 'M': 1e6, 'B': 1e9}
+    val = val.replace('$', '')
+    last_char = val[-1]
+    if last_char in multiplyer:
+        # print(val[:-1])
+        val = float(val[:-1]) * multiplyer[last_char]
+    return val
+
+
+def _parse_pct(val):
+    val = val.replace('%', '')
+    return float(val)
+
+
+def parse(val):
+    if '%' in val:
+        return _parse_pct(val)
+    else:
+        return _parse_cnt_price(val)
+
+
+def compare(val1, val2):
+    val1 = parse(val1)
+    val2 = parse(val2)
+    return val1 > val2
+
+
+headers = {
+    'User-Agent': 'My User Agent 1.0',
+    'From': 'youremail@domain.example',  # This is another valid field
+    # 'cookie': 'calendar-countries=usa,tun; calendar-importance=3; ASP.NET_SessionId=d; cal-timezone-offset=180; TEServer=TEIIS'
+    # ASP.NET_SessionId=pdl0gpiioeziec4ezsg5pgui
+    # calendar-range=3;
+    # WORKING
+    'cookie': 'calendar-countries=usa, eu; calendar-importance=2; calendar-range=1;  TEServer=TEIIS'
+    # last week: calendar-range=-2; ## THIS WEEK: calendar-range=3;
+}
+
+
+def strip_and_clean(x):
+    x = x.getText().replace('\r\n', '').replace('\n', '').replace('®', '').strip()
+    return ' '.join(x.split())
+
+
+def scrape_data():
+    r = requests.get(url, headers=headers)
+    soup = bs(r.content)
+    table = soup.find('table', {'id': 'calendar'})
+    # cols = ['dt', 'country', 'title', 'actual', 'previous', 'consensus', 'forecast', 'compare']
+    cols = ['importance', 'dt', 'country', 'title', 'actual', 'consensus', 'compare']
+    res_lst = []
+    for child in table.children:
+        if isinstance(child, str):
+            continue
+        if child.get_attribute_list('class') == ['table-header']:
+            #         print([strip_and_clean(x) for x in child.find_all(['th'])])
+            date = strip_and_clean(child.find_all(['th'])[0])
+        if child.has_attr('data-category'):
+            entry = [strip_and_clean(x) for x in
+                     child.find_all(['td'])]  # # [x for x in child.find_all(['td']) if x.attrs]
+            try:
+                importance = child.find_all(['td'])[0].contents[1].attrs['class'][0].split('-')[-1]
+            except:
+                print('importance extraction failed..')
+                importance = 10
+            time = entry[0]
+            country = entry[1]
+            title = entry[4]  # .strip()
+            actual = entry[5]
+            #         previous = entry[6]
+            consensus = entry[7]
+            #         forecast = entry[8]
+            dt = pd.to_datetime(f'{date} {time}')
+            print(actual, consensus)
+            _compare = None
+            try:
+                _compare = compare(actual, consensus) if len(actual) else None
+            except Exception as e:
+                print(f'Could not compare: {actual, consensus}', e)
+            res = importance, dt, country, title, actual if len(actual) > 0 else 'NOTYET', consensus, _compare
+            res_lst.append(res)
+    #         print(','.join(res))
+    #         print([strip_and_clean(x) for x in child.find_all(['td'])])
+    df = pd.DataFrame(res_lst, columns=cols)
+    return df
