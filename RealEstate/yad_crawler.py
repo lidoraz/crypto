1cc9f6f60a694506b555d3927a3df4ed48084b66
shushu <shushu33434@gmail.com>
2023-01-22 21:54:47 +0200

    added yad2 crawler

--- RealEstate/yad_crawler.py
+++ RealEstate/yad_crawler.py
@@ -10,19 +10,16 @@ from RealEstate.scraper_yad2 import scraper_yad2
 
 
 def _scraper():
-    with pg.connect(
-            host="localhost",
-            port=5432,
-            user=os.getenv("DB_USER"),
-            password=os.getenv("DB_PASS"),
-            database="vsdatabase"
-    ) as conn:
+    from sqlalchemy import create_engine
+    engine = create_engine(f'postgresql://{os.getenv("DB_USER")}:{os.getenv("DB_PASS")}@localhost:5432/vsdatabase')
+    with engine.connect() as conn:
         print(f"{datetime.today()} Starting to fetch!")
         scraper_yad2(conn)
         print(f"{datetime.today()} Finished !")
 
 
 def routine_daily_yad2_to_db():
+    _scraper()
     schedule.every().day.at("00:00").do(_scraper)
     while True:
         schedule.run_pending()
