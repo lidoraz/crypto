d83b016831b5268a3234c7cdf9e7d012005eaf4b
shushu <shushu33434@gmail.com>
2023-02-19 19:53:00 +0200

    added yad2 crawler for rent as well

--- RealEstate/scraper_yad2.py
+++ RealEstate/scraper_yad2.py
@@ -1,13 +1,10 @@
 import os
 
-import numpy as np
 import requests
-import schedule
 from tqdm import tqdm
 import time
 import pandas as pd
 from datetime import datetime
-import sqlite3
 
 import sqlalchemy
 
@@ -19,6 +16,7 @@ history_dtype = {
 url_forsale_apartments_houses = "https://gw.yad2.co.il/feed-search-legacy/realestate/forsale?propertyGroup=apartments,houses&page={}&forceLdLoad=true"
 url_rent_apartments_houses = "https://gw.yad2.co.il/feed-search-legacy/realestate/rent?propertyGroup=apartments,houses&page={}&forceLdLoad=true"
 TRIES = 5
+N_THREADS_ADD_INFO = 6
 
 forsale_today_cols = ['line_1', 'line_2', 'line_3', 'row_1', 'row_2', 'row_3', 'row_4',
                       'search_text', 'title_1', 'title_2', 'images_count', 'img_url',
@@ -70,13 +68,49 @@ def _process_price(x):
         return x.replace(',', '').replace(' ₪', '').replace(' $', '')
 
 
+def _get_parse_item_add_info(item_id):
+    yad2_url_item = "https://gw.yad2.co.il/feed-search-legacy/item?token={}"
+    try:
+        d = requests.get(yad2_url_item.format(item_id)).json()['data']
+        is_bad = d.get('error_message') is not None
+        if is_bad:
+            return None
+        items_v2 = {x['key']: x['value'] for x in d['additional_info_items_v2']}
+        add_info = dict(id=item_id,
+                        parking=0 if d['parking'] == "ללא" else int(d['parking']),
+                        balconies=True if d['balconies'] else False,
+                        number_of_floors=d['TotalFloor_text'],
+                        renovated=items_v2.get('renovated'),
+                        asset_exclusive_declaration=items_v2.get('asset_exclusive_declaration'),
+                        air_conditioner=items_v2.get('air_conditioner'),
+                        bars=items_v2.get('bars'),
+                        elevator=items_v2.get('elevator'),
+                        boiler=items_v2.get('boiler'),
+                        accessibility=items_v2.get('accessibility'),
+                        shelter=items_v2.get('shelter'),
+                        warhouse=items_v2.get('warhouse'),
+                        tadiran_c=items_v2.get('tadiran_c'),
+                        furniture=items_v2.get('furniture'),
+                        flexible_enter_date=items_v2.get('flexible_enter_date'),
+                        kosher_kitchen=items_v2.get('kosher_kitchen'),
+                        housing_unit=items_v2.get('housing_unit'),
+                        info_text=d['info_text'],
+                        image_urls=d['images_urls']
+                        )
+        return add_info
+    except Exception as e:
+        print(f"Failed to fetch for {item_id}")
+    return None
+
+
 class ScraperYad2:
-    def __init__(self, url, use_cols, today_table, history_table, log_table):
+    def __init__(self, url, use_cols, today_table, history_table, log_table, item_table):
         self.url = url
         self.use_cols = use_cols
         self.history_table = history_table
         self.today_table = today_table
         self.log_table = log_table
+        self.item_table = item_table
 
     def _get_retry_json(self, p):
         res = None
@@ -161,6 +195,24 @@ class ScraperYad2:
         con.execute(
             f"CREATE TABLE IF NOT EXISTS {self.history_table}(id VARCHAR(255) not null, price float, date TIMESTAMP, date_added TIMESTAMP not null, processing_date DATE not null)")
 
+    def insert_to_items(self, con):
+        ids = pd.read_sql(f"SELECT id from {self.today_table}", con)['id'].to_list()
+        if not sqlalchemy.inspect(con).has_table(self.item_table):
+            df_items = pd.Series(self._get_parse_item_add_info(ids[0])).to_frame()
+            df_items.to_sql(self.item_table, con)
+            con.execute(f"ALTER TABLE {self.item_table} ADD PRIMARY KEY (id);")
+
+        ids_in_items = pd.read_sql(f"SELECT id from {self.item_table}", con)['id'].to_list()
+        ids_to_insert = list(set(ids_in_items) - set(ids_in_items))
+        from concurrent.futures import ThreadPoolExecutor
+        with ThreadPoolExecutor(N_THREADS_ADD_INFO) as executor:
+            futures_to_id = [executor.submit(_get_parse_item_add_info, item) for item in ids_to_insert]
+            results = [f.result() for f in futures_to_id]
+        results = [i for i in results if i is not None]
+        df_items = pd.DataFrame(results)
+        df_items.to_sql(self.item_table, con, index=False)
+        print(f"insert_to_items: Inserted to {self.item_table} {len(df_items)} items!")
+
     def scraper_yad2(self, con):
         self.create_tables(con)
         res = self._get_retry_json(1)
@@ -178,8 +230,10 @@ class ScraperYad2:
             self.log_history(df, df_today_history, con)
             self.insert_today_temp(df, con)
         if df is not None:
+            # update only if there was a change(prob was)
             self.update_today(con)
             self.track_active(con)
+            self.insert_to_items(con)
 
     def _check_exists(self, today_str, con):
         cnt_today = pd.read_sql(f"SELECT count(*) from {self.history_table} where processing_date = '{today_str}'",
@@ -206,7 +260,8 @@ def get_scraper_yad2_forsale():
     scraper = ScraperYad2(url_forsale_apartments_houses, forsale_today_cols,
                           "yad2_forsale_today",
                           "yad2_forsale_history",
-                          'yad2_forsale_log')
+                          'yad2_forsale_log',
+                          "yad2_forsale_items")
     return scraper
 
 
@@ -214,7 +269,8 @@ def get_scraper_yad2_rent():
     scraper = ScraperYad2(url_rent_apartments_houses, rent_today_cols,
                           "yad2_rent_today",
                           "yad2_rent_history",
-                          'yad2_rent_log')
+                          'yad2_rent_log',
+                          "yad2_rent_items")
     return scraper
 
 
