12a2cc78383556b9fb4ede286bbde2c33c1974a3
shushu <shushu33434@gmail.com>
2023-03-25 16:09:58 +0300

    added insiders, fixxx

--- AStock/insider_buy.py
+++ AStock/insider_buy.py
@@ -53,10 +53,10 @@ def filter_stocks(df, avg_volume_m=2, minimum_price=10):
 
 
 def run(minimum_price=5, avg_volume_m=2):
-    # df = get_insiders()
-    # df = filter_stocks(df, avg_volume_m=avg_volume_m, minimum_price=minimum_price)
+    df = get_insiders()
+    df = filter_stocks(df, avg_volume_m=avg_volume_m, minimum_price=minimum_price)
     # df.to_pickle('tmp.pk')
-    df = pd.read_pickle('tmp.pk')
+    # df = pd.read_pickle('tmp.pk')
     display(df)
 
 
