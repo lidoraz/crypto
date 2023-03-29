86d14946f1d96d851a8da451a75d5ab2bdd6a47b
shushu <shushu33434@gmail.com>
2023-03-29 14:26:02 +0300

    added insiders, fixxxx

--- AStock/insider_buy.py
+++ AStock/insider_buy.py
@@ -192,9 +192,9 @@ def pub_object(path_from, path_to):
 
 
 def run(minimum_price=5, avg_volume_m=2):
-    # df = get_insiders()
-    # df = filter_stocks(df, avg_volume_m=avg_volume_m, minimum_price=minimum_price)
-    # df.to_pickle('tmp.pk')
+    df = get_insiders()
+    df = filter_stocks(df, avg_volume_m=avg_volume_m, minimum_price=minimum_price)
+    df.to_pickle('tmp.pk')
     df = pd.read_pickle('tmp.pk')
     df = preprocess(df)
     create_html(df)
