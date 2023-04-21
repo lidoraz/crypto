a0e164ce65248166a9317652c0e947eca0d87e13
shu <shu@gmail.com>
2023-04-21 13:34:52 +0300

    some styling

--- AStock/get_swing_stocks.py
+++ AStock/get_swing_stocks.py
@@ -344,9 +344,9 @@ def get_swings(test=False):
     tickers += high_growth + indexes + sector_tickers
     tickers = list(set(tickers))
 
-    # data = get_data_retry(tickers)
-    # df = check_ticker(data)
-    # df.to_pickle("data_swing.pk")
+    data = get_data_retry(tickers)
+    df = check_ticker(data)
+    df.to_pickle("data_swing.pk")
     df = pd.read_pickle("data_swing.pk")
 
     df_index = df.reindex(indexes)  # filter and reindex
